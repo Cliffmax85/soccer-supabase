31fc3c7b29c7e07b1ade7c6cdb70570eec40328b
Clifford Maxson <Cliffmax85@gmail.com>
2022-01-07 16:31:19 -0800

    Update example.test.js

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,47 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { renderTeam, renderGame } from '../render-utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('rendergame takes an argument and returns 2 divs with name and score', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const game = {
+        name1: 'cow',
+        score1: 3,
+        name2: 'jeffy',
+        score2: 2
+    };
+
+    const expected = '<div class="game"><div class="team"><p class="name">cow</p><p class="score">3</p></div><div class="team"><p class="name">jeffy</p><p class="score">2</p></div></div>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderGame(game);
 
     //Expect
+    expect.equal(actual.outerHTML, expected, 'return a div with 4 p tags');
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    //expect.equal(actual, expected);
+});
+
+
+test('renderteam returns a dom node with 2 p tags name and score', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const team = {
+        name1: 'cow',
+        score1: 3
+    };
+
+    const expected = '<div class=\"team\"><p class=\"name\">[object Object]</p><p class=\"score\"></p></div>';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderTeam(team);
+
+    //Expect
+    expect.equal(actual.outerHTML, expected, 'return a div with 2 p tags name and score');
+    // Make assertions about what is expected versus the actual result
+    //expect.equal(actual, expected);
 });
